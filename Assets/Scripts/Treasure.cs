3e237dfbf8a6d1d906ba249f8379e6874282b3d4
MrDallOca <tdalloca@gmail.com>
2017-11-08 15:32:01 -0200

    Single commit

---
+++ Assets/Scripts/Treasure.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System;
+
+public class Treasure : MonoBehaviour {
+
+	public int ultimo_level = 3;
+	public int proximo_nivel = 5;
+	public int value = 10;
+	public GameObject explosionPrefab;
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.tag == "Player") {
+			if (GameManager.gm!=null)
+			{
+				// tell the game manager to Collect
+				GameManager.gm.Collect (value);
+				// Debug.Log(GameManager.gm.score);
+				// if (GameManager.gm.score >= proximo_nivel){
+
+				// 	var num  = Convert.ToInt32(SceneManager.GetActiveScene().name.Substring(4,2));
+				// 	num++;
+
+				// 	Debug.Log(Convert.ToString(ultimo_level) + "," + Convert.ToString(num));
+
+				// 	if (num <= ultimo_level)
+				// 		Application.LoadLevel("cena0" + num);
+						
+				// }
+			}
+			
+			// explode if specified
+			if (explosionPrefab != null) {
+				Instantiate (explosionPrefab, transform.position, Quaternion.identity);
+			}
+			
+			// destroy after collection
+			Destroy (gameObject);
+		}
+	}
+}
